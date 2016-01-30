c7a11af876c1de95a33e0722ab72b9e52cb69e05
hspgus <hspgus@gmail.com>
2016-01-30 16:31:47 -0700

    message

---
+++ conditional.py
@@ -0,0 +1,8 @@
+x = 7
+day = "Friday"
+if "connor":
+    print("The day is " + day + 
+          ". What a great day.")
+else:
+    print("I don't think this will happen")
+    
---
+++ literation.py
@@ -0,0 +1,4 @@
+books = ["Tale of Two Cities", "Harry Potter", "Lord of the Rings" ]
+nums = [0, 1, 2, 3]
+
+print(books[0])
